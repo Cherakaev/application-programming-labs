b97ae9245f8a03c4741426b38f7a4cbd6acc453f
Cherakaev <cherakaevv.yarik@gmail.com>
2024-10-27 04:33:33 +0400

    5th: changed last function and fixed text.strip()

--- lab_1/main.py
+++ lab_1/main.py
@@ -1,5 +1,5 @@
-import re
 import argparse
+import re
 
 
 def get_filepath() -> str:
@@ -21,16 +21,24 @@ def read_file(file_path: str) -> str:
 def split_forms(text: str) -> list[str]:
     #function splits string into list_of_strings
     pattern = r'\d+[)]'
-    text.strip()
+    text = text.strip()
     list_of_forms: list[str] = re.split(pattern, text)
     return list_of_forms
 
 
-def print_moscow(list_of_forms: list[str]):
-    #function prints all forms with Москва in it
+def find_moscow(list_of_forms: list[str]) -> list[str]:
+    #function return list of forms with Москва in it
+    new_list_of_forms = []
     for form in list_of_forms:
         if 'Москва' in form:
-            print(form.strip(), '\n')
+            new_list_of_forms.append(form)
+    return new_list_of_forms
+
+
+def print_forms(list_of_forms: list[str]):
+    #function prints all forms
+    for form in list_of_forms:
+        print(form.strip(), '\n')
 
 
 def main() -> None:
@@ -38,7 +46,8 @@ def main() -> None:
         filepath = get_filepath()
         text = read_file(filepath)
         list_of_forms = split_forms(text)
-        print_moscow(list_of_forms)
+        list_of_forms = find_moscow(list_of_forms)
+        print_forms(list_of_forms)
     except Exception as exc:
         print(f"Error: {exc}")
 
